411bf42469d814398f87366d228d10dc8da2e49b
Михайло Кондратенко <mike.kondr@gmail.com>
2023-05-03 23:03:09 +0300

    Task1

--- task1/Form1.Designer.cs
+++ task1/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            listBox1 = new ListBox();
+            propertyGrid1 = new PropertyGrid();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(12, 12);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(349, 224);
+            listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // propertyGrid1
+            // 
+            propertyGrid1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            propertyGrid1.HelpVisible = false;
+            propertyGrid1.Location = new Point(12, 242);
+            propertyGrid1.Name = "propertyGrid1";
+            propertyGrid1.Size = new Size(349, 258);
+            propertyGrid1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(373, 512);
+            Controls.Add(propertyGrid1);
+            Controls.Add(listBox1);
+            Name = "Form1";
+            Text = "Programmers XML";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox listBox1;
+        private PropertyGrid propertyGrid1;
     }
 }
--- task1/Form1.cs
+++ task1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+
 namespace task1
 {
     public partial class Form1 : Form
@@ -5,6 +7,28 @@ namespace task1
         public Form1()
         {
             InitializeComponent();
+            LoadEmployees();
+        }
+
+        public void LoadEmployees()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("employees.xml");
+
+            foreach (XmlNode node in doc.DocumentElement)
+            {
+                string name = node.Attributes[0].Value;
+                int age = int.Parse(node["Age"].InnerText);
+                bool programmer = bool.Parse(node["Programmer"].InnerText);
+
+                listBox1.Items.Add(new Employee(name, age, programmer));
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+                propertyGrid1.SelectedObject = listBox1.SelectedItem;
         }
     }
 }
